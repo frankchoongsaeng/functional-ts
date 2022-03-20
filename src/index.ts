5e3e10723cdf884a4099c087bf08cfe0c12a6cf4
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2022-03-20 15:26:52 +0000

    initial implementation of the pipe function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-class Pipeline {
-    
+const pipe : any = (initExpression: any, ...args: Array<any>) => {
+    let e = initExpression;
+
+    for (const arg of args) {
+        
+        // arg could be a function 
+        if (typeof arg === 'function') {
+            arg(e)
+        }
+        
+        if (Array.isArray(arg)) {
+            arg[0](...arg.slice(1))
+        }
+    }
 }
