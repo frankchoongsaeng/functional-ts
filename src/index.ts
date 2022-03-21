4aa8c00ae26018d247cec97a32cce3f40ce17aad
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2022-03-21 22:04:10 +0000

    small cosmetic changes

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
  *
  * @example
  *      pipe (null, fn1, fn2, fn3) returns the final output of fn3
- *      pipe ("starting value", fn1, [fn2, "fn2 value"]) 
+ *      pipe ("starting value", fn1, [fn2, "fn2 value"])
  * @param {any} initExpression  an initial value to pipe into the first function as a last param
  * @param {Function | Array} args a list of functions that will get called serially with the output of the previous function as a last input
  * @returns {any}
@@ -19,11 +19,9 @@ export const pipe: any = (
 
     for (const arg of args) {
         // arg could be a function
-        if (typeof arg === 'function') {
-            v = arg(v)
-        } else if (Array.isArray(arg)) {
-            v = arg[0](...arg.slice(1), v)
-        } else
+        if (typeof arg === 'function') v = arg(v)
+        else if (Array.isArray(arg)) v = arg[0](...arg.slice(1), v)
+        else
             throw new Error(
                 "Invalid arg: expected 'function' but found " + typeof arg
             )
